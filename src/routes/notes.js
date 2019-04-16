e3c438bb497aea1aba3d0751d08f75f567b37de9
JarvisFullStack <39954886+JarvisFullStack@users.noreply.github.com>
2019-04-16 10:59:07 -0400

    CRUD de notas completado.

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -27,6 +27,7 @@ router.post('/notes/new-note', async (req, res) => {
     } else {
         const newNote = new Note({title, description});
         await newNote.save();
+        req.flash('success_msg', 'La nota se agregó de manera exitosa!');
         res.redirect('/notes');
     }
     
@@ -42,6 +43,20 @@ router.get('/notes/edit/:id', async (req, res) => {
     res.render('notes/edit-note', {
         note
     });
+});
+
+router.put('/notes/edit-note/:id', async (req, res) => {
+    const {title, description} = req.body;
+    await Note.findByIdAndUpdate(req.params.id, {title, description});
+    req.flash('success_msg', 'Nota Actualizada correctamente!');
+    res.redirect('/notes');
+});
+
+
+router.delete('/notes/delete/:id', async (req, res) => {
+    await Note.findByIdAndDelete(req.params.id);
+    req.flash('success_msg', 'Nota eliminada correctamente!');
+    res.redirect('/notes');
 });
 
 module.exports = router;
